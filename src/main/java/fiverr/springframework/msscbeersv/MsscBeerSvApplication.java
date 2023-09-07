1982d318456a944f542a4e43cee17540307e0677
Ayoub <wwzera05@gmail.com>
2023-09-07 14:58:10 +0200

    Fiest commit

---
+++ src/main/java/fiverr/springframework/msscbeersv/MsscBeerSvApplication.java
@@ -0,0 +1,13 @@
+package fiverr.springframework.msscbeersv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerSvApplication.class, args);
+	}
+
+}
